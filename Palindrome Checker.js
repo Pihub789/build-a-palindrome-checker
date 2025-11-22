9a1d1b8eb8039c01f1106cc42f9c7635dee658ac
Pihub789 <pragneshpatel7@yahoo.com>
2025-11-22 17:54:21 -0500

    Add files via upload

---
+++ Palindrome Checker.js
@@ -0,0 +1,19 @@
+function checkPalindrome(){
+    const inputText = document.getElementById("Text-input").value
+    if (!inputText){
+        alert("Please input a value")
+    }
+    const result = document.getElementById("result");
+    const inputArr = inputText.toLowerCase().split("").filter((c) => !c.match(/[\(\)\\.\s_,-]/gi));
+
+    console.log(inputArr)
+    for(let i=0; i<inputArr.length; i++){
+        if(inputArr[i]!== inputArr[inputArr.length - i - 1]) {
+            console.log(inputArr[i])
+            result.innerText = "${inputText} is not a palindrome"
+        }
+    }
+
+    
+}
+
